795e760b59437655e9030b132d45b1f0f5eb172b
saignanadeep11 <112959877+saignanadeep11@users.noreply.github.com>
2024-05-05 17:24:18 +0530

    Time: 30 ms (98.62%) | Memory: 16.5 MB (82.64%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.py
+++ 1503-reducing-dishes/reducing-dishes.py
@@ -1,16 +1,11 @@
 class Solution:
     def maxSatisfaction(self, nums: List[int]) -> int:
         nums.sort()
-        # print(nums)
-        n=len(nums)
-        ans=-1
-        sum=0
-        for i in range(n):
-            sum=0
-            id=0
-            for j in range(i-1,n):
-                sum+=nums[j]*id
-                ans=max(ans,sum)
-                id+=1
-                # print(nums[j],sum,ans)
+        ans=cnt=0
+        while nums:
+            cur=nums.pop()
+            if cur+cnt<0:
+                return ans
+            cnt+=cur
+            ans+=cnt
         return ans
