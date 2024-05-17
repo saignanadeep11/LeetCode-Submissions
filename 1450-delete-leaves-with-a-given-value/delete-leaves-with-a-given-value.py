78cf8ca781dce5ac2452c615503afff49a2a8f1b
saignanadeep11 <112959877+saignanadeep11@users.noreply.github.com>
2024-05-17 09:51:42 +0530

    Time: 49 ms (29.74%) | Memory: 17.1 MB (28.92%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -6,23 +6,12 @@
 #         self.right = right
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        
-        def rem(root):        
-          if root==None or root.left==None and root.right==None:
-            return 
-          
-          rem(root.left)and rem(root.right)
-          if(root.left!=None):
-            if root.left.val==target and root.left.left==None and root.left.right==None:
-              root.left=None
-          if root.right!=None:
-            if root.right.val==target and root.right.left==None and root.right.right==None:
-              root.right=None
-          rem(root.left)
-          rem(root.right)
-        rem(root)
-        rem(root)
-        rem(root)
-        if root!=None and root.left==None and root.right==None and root.val==target:
-          return 
+        if root==None:
+          return
+        if not root.left and not root.right and root.val==target:
+          return None
+        root.left=self.removeLeafNodes(root.left,target)
+        root.right=self.removeLeafNodes(root.right,target)
+        if not root.left and not root.right and root.val == target:
+              return
         return root
