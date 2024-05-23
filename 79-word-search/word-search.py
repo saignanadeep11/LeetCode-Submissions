1b631022c9dfd60c3c174be59e9ab42c1c0ebffc
saignanadeep11 <112959877+saignanadeep11@users.noreply.github.com>
2024-05-23 13:04:10 +0530

    Time: 3326 ms (76.77%) | Memory: 16.5 MB (70.75%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -4,25 +4,24 @@ class Solution:
         n=len(board)
         m=len(board[0])
         wordlen=len(word)
-        def dp(i,j,k):
+        def dp(i,j,k,flag):
             # print(i,j,k)
             if(k>=wordlen):
                 return True
-            if(i<0 or j<0 or i>=n or j>=m or board[i][j]=='$'):
+            if(i<0 or j<0 or i>=n or j>=m or flag[i][j]==-1):
                 return False
             
             if(board[i][j]==word[k]):
-                temp=board[i][j]
-                board[i][j]='$'
-                ans=(dp(i-1,j,k+1)or dp(i+1,j,k+1)or dp(i,j-1,k+1)or dp(i,j+1,k+1) )
-                board[i][j]=temp
+                flag[i][j]=-1
+                ans=(dp(i-1,j,k+1,flag)or dp(i+1,j,k+1,flag)or dp(i,j-1,k+1,flag)or dp(i,j+1,k+1,flag) )
+                flag[i][j]=0
                 return ans
             return False
         for i in range(n):
             for j in range(m):
                 
                 if board[i][j]==word[0]:
-                    # flag=[[0 for i in range(m)] for i in range(n)]
-                    if(dp(i,j,0)):
+                    flag=[[0 for i in range(m)] for i in range(n)]
+                    if(dp(i,j,0,flag)):
                         return True
         return False
