8e7c3bf37563c264fe290af8f8ad450fc09a17b7
saignanadeep11 <112959877+saignanadeep11@users.noreply.github.com>
2024-05-24 11:37:25 +0530

    Time: 44 ms (14.52%) | Memory: 16.5 MB (88.51%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.py
+++ 926-find-and-replace-pattern/find-and-replace-pattern.py
@@ -1,20 +1,10 @@
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
-        if ["badc","abab","dddd","dede","yyxx"]==words and pattern=="baba":
-            return ['abab','dede']
+        
         n=len(words)
         m=len(pattern)
         ans=[]
-        count=[0 for _ in range(27)]
-        for i in range(m):
-            count[ord(pattern[i])-97]+=1
-        for i in range(n):
-            c=0
-            for j in range(m):
-                # c+=1
-                # print(words[i].count(words[i][j]),count[ord(pattern[j])-97])
-                if words[i].count(words[i][j])==count[ord(pattern[j])-97]:
-                    if(c>=m-1):
-                        ans.append(words[i])
-                    c+=1
+        for i in words:
+            if len(set(i))==len(set(pattern))==len(set(zip(i,pattern))):
+                ans.append(i)
         return ans
