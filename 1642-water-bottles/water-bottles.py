f7dd3643c052d60282d4e36c7f9079efa30b4d0e
saignanadeep11 <112959877+saignanadeep11@users.noreply.github.com>
2024-07-07 10:44:43 +0530

    Time: 36 ms (46.25%) | Memory: 16.4 MB (97.50%) - LeetSync

--- 1642-water-bottles/water-bottles.py
+++ 1642-water-bottles/water-bottles.py
@@ -1,7 +1,6 @@
 class Solution:
     def numWaterBottles(self, n: int, m: int) -> int:
-        ans=0
-        ans+=n
+        ans=n
         while(n>=m):
             ans+=n//m
             n=(n%m)+(n//m)
