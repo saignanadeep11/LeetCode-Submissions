4ab1b8bac38864c7ef3576c922b71ecb36d69da9
saignanadeep11 <112959877+saignanadeep11@users.noreply.github.com>
2024-07-21 16:34:27 +0530

    Time: 58 ms (25.07%) | Memory: 49.1 MB (12.03%) - LeetSync

--- 2733-sleep/sleep.js
+++ 2733-sleep/sleep.js
@@ -3,8 +3,8 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-    var k=new Promise(res=>setTimeout(res,millis));
-    return k;
+    await new Promise(res=>setTimeout(res,millis));
+    
 }
 
 /** 
