acd59cca6c7e177683aa6adedea1efee1fa16dee
saignanadeep11 <112959877+saignanadeep11@users.noreply.github.com>
2024-07-21 10:31:33 +0530

    Time: 50 ms (79.11%) | Memory: 48.9 MB (85.84%) - LeetSync

---
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let k=init;
+    for(let i=0;i<nums.length;i++){
+        k=fn(k,nums[i]);
+    }
+    return k;
+};
