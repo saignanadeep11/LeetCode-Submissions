0f71597acc37fff6ca62b43a012e3d95d6c6a834
saignanadeep11 <112959877+saignanadeep11@users.noreply.github.com>
2024-07-21 16:21:44 +0530

    Time: 71 ms (20.03%) | Memory: 49.3 MB (73.30%) - LeetSync

---
+++ 2859-add-two-promises/add-two-promises.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+
+    let [v1,v2]=await Promise.all([promise1,promise2]);
+    return v1+v2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
