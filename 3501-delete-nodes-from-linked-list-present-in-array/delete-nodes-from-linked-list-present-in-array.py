bb5740e735d99bd5a96f66533e3fb4c1ec49796d
saignanadeep11 <112959877+saignanadeep11@users.noreply.github.com>
2024-09-06 20:01:49 +0530

    Time: 721 ms (56.98%) | Memory: 54.6 MB (81.13%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
@@ -7,14 +7,14 @@ class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
         ans=head
         pre=None
-        m=max(nums)
-        fre=[0]*(m+1)
+        # m=max(nums)
+        fre=set(nums)
         # print(fre)
-        for i in nums:
-            fre[i]=1
+        # for i in nums:
+        #     fre[i]=1
         while(head):
             # print(head.val,1)
-            if(head.val<=m and fre[head.val]==1):
+            if(head.val in fre):
                 if head.next:
                     head.val=head.next.val
                     head.next=head.next.next
