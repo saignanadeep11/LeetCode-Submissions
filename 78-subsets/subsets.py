233ed792190ed94d8016b12f3df18de766b716c4
saignanadeep11 <112959877+saignanadeep11@users.noreply.github.com>
2025-01-03 12:10:11 +0530

    Time: 3 ms (10.60%) | Memory: 17.9 MB (10.38%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,14 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans=[]
-        def recursive(nums,idx,small,n):
-            if(idx>=n):
-                ans.append(small[:])
-                return
-            small+=[nums[idx]]
-            recursive(nums,idx+1,small,n)
-            small.pop()
-            recursive(nums,idx+1,small,n)
-            
-        recursive(nums,0,[],len(nums))
+        n=len(nums)
+        arrLen=pow(2,n)
+        for i in range(arrLen):
+            sarr=[]
+            for j in range(32):
+                if ((i)&(1<<j)):
+                    sarr.append(nums[j])
+            ans.append(sarr)
         return ans
